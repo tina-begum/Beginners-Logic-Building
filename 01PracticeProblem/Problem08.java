8e02ee4126d08da84b22d45e102255d0579ef3a7
tina-begum <quantcom01@gmail.com>
2026-08-04 23:04:10 +0530

    Basic logic building problems

--- 01PracticeProblem/Problem08.java
+++ 01PracticeProblem/Problem08.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class Problem08 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int num = sc.nextInt();
         int firstTerm = 0;
         int secondTerm = 1;
         int sum = 0;
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= num; i++) {
             System.out.print(firstTerm + " ");
             sum = firstTerm + secondTerm;
             firstTerm = secondTerm;
--- 01PracticeProblem/Problem09.java
+++ 01PracticeProblem/Problem09.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class Problem09 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        int originalNum = num;
+        int n = sc.nextInt();
+        int originalNum = n;
         int sum = 0;
-        for (int i = 1; i < num; i++) {
-            if (num % i == 0) {
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0) {
                 sum += i;
             }
         }
